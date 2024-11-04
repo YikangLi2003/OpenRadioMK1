fefb92fd0e35a74d013e125ff5cccac21930977b
yikangl <liyikang1024@163.com>
2024-11-04 20:55:26 +1100

    To commemorate a 'groundbreaking' hardware project

---
+++ firmware src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    /*
+        "You might be wondering why there’s no firmware code here.
+        Well, let me explain......
+        After plugging in the power,
+        the components on the PCB didn’t exactly ‘power up’
+        so much as release its magical smoke... not that I’m particularly surprised...
+        As someone far better at writing code than designing electronic circuits,
+        let me to ease the awkwardness with a humble ‘Hello, World!’ in the standard output."
+
+        -------- 2024 Nov 4 before I archived the repo.
+    */
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
